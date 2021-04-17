8b46e51c2420505c8b98a5af1808033316446102
baochung26 <phungbaochung@gmail.com>
2021-04-18 00:57:30 +0700

    search task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,28 @@
-import {Controller, Get, Post, Body, Param, Delete, Put, Patch} from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Patch,
+  Query,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter-dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private TasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
+  getAllTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    if (Object.keys(filterDto).length) {
+      return this.TasksService.getTasksWithFilters(filterDto);
+    }
+    console.log(filterDto);
     return this.TasksService.getAllTask();
   }
 
