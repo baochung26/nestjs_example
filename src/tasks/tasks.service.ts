37cff9b0ebb1d9dfd043508df43e9db481d41dff
baochung26 <phungbaochung@gmail.com>
2021-04-17 21:40:39 +0700

    nestjs example

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v1 as uuid } from 'uuid';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTask(): Task[] {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string) {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
+  }
+}
